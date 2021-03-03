2bee8ebdf968b729ff90dbd3a32a1b2419184ec7
Thao Ma <MaThao@gmail.com>
2021-03-03 16:58:54 -0700

    Add assets and add img to header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,9 +3,9 @@ import Header from '../Header/Header';
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+    </>
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import leafIcon from '../../assets/leaf-icon.png';
 
 const Header = () => {
 
   return (
     <header>
        <h1>Animal Crossing</h1>
+       <img src={leafIcon} />
+       <input
+          type='text'
+          placeholder='Search Villagers'
+          aria-label='Search Villagers'
+        />
     </header>
   )
 }
