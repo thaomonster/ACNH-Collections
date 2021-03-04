2a8036af8d694852b43a7b588acdf07489eb6987
Thao Ma <MaThao@gmail.com>
2021-03-03 19:09:21 -0700

    Move useStates inside of App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import { getAllVillagers } from '../../apiCall';
 
-const [ villagers, setVillagers] = useState([]);
 
 const App = () => {
+  const [villagers, setVillagers] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  
   return (
     <>
       <Header />
