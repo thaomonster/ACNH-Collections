31a1f8b8978f2c8748c36366d3b4124609a07cab
Thao Ma <MaThao@gmail.com>
2021-03-04 14:46:08 -0700

    Change Hover effect to only img and name

--- src/Components/VillagerList/VillagerList.js
+++ src/Components/VillagerList/VillagerList.js
@@ -6,11 +6,13 @@ const VillagerList = ({villagers}) => {
   const card = villagers.map(villager => {
     return (
       <section key={villager.id} id={villager.id} >
-        <div className='card'>
-          <img src={villager.icon_uri} alt='villager icon' />
-          <p>{villager.name['name-USen']}</p>
+        <article className='card'>
+          <div className='thumbnail-img'>
+            <img src={villager.icon_uri} alt='villager icon' />
+            <p>{villager.name['name-USen']}</p>
+          </div>
           <img className='card-leaf' src={leafIcon} alt='Leaf Icon' />
-        </div>
+        </article>
       </section>
     )
   })
