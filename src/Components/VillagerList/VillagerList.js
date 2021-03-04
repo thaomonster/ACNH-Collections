5f5680e13df81d9559021046814151ea6e25ffa8
Thao Ma <MaThao@gmail.com>
2021-03-04 15:38:43 -0700

    Change leaf from white to blue

--- src/Components/VillagerList/VillagerList.js
+++ src/Components/VillagerList/VillagerList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import leafGreen from '../../assets/leaf-green.png';
-import leafWhite from '../../assets/leaf-white.png';
+import leafBlue from '../../assets/leaf-blue.png';
 
 const VillagerList = ({villagers}) => {
 
@@ -14,7 +14,7 @@ const VillagerList = ({villagers}) => {
           </div>
           <img 
             className='card-leaf' 
-            src={leafWhite} 
+            src={leafBlue} 
             alt='Leaf Icon'
             />
         </article>
