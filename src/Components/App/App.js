36b53dd16c9b415d676fa0e2c8b7976f186e3c20
Thao Ma <MaThao@gmail.com>
2021-03-03 19:29:25 -0700

    Move api calls to VillagerList from App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
-import { getAllVillagers } from '../../apiCalls';
 
 
 const App = () => {
-  const [villagers, setVillagers] = useState([]);
-  const [favorites, setFavorites] = useState([]);
-
-
+  
   return (
     <>
       <Header />
---
+++ src/Components/VillagerList/VillagerList.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { getAllVillagers } from '../../apiCalls';
+
+const VillagerList = () => {
+  const [villagers, setVillagers] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+
+}
