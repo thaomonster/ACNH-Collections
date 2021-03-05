e0935fd614a4818595796cca1ee67cf2e7a7fd7f
Thao Ma <MaThao@gmail.com>
2021-03-04 23:06:25 -0700

    Add passport background

--- src/Components/ProfilePage/ProfilePage.js
+++ src/Components/ProfilePage/ProfilePage.js
@@ -15,22 +15,22 @@ const ProfilePage = ({match}) => {
       <section className='passport-container'>
         <h2>--- PASSPORT ---</h2>
         <article className='profile-page'>
-          <img 
-            src={selectedVillager.image_uri} 
-            alt='villager profile' 
-            className='profile-picture'
-            />
-         <div className='info'>
-            <p>Personality: {selectedVillager.personality}
-              <span className='gender'>{String.fromCodePoint(0x26A7)} {selectedVillager.gender}</span>
-            </p>
-            <p className='species'>Species: {selectedVillager.species}</p>
-            <p className='name'>Name: {selectedVillager.name['name-USen']}</p>
+          <div className='left-side'>
+            <img 
+              src={selectedVillager.image_uri} 
+              alt='villager profile' 
+              className='profile-picture'
+              />
+            <p>Name: {selectedVillager.name['name-USen']}</p>
             <p>{String.fromCodePoint(0x1F330)}Born: {selectedVillager['birthday-string']}</p>
-            <div className='sayings'>
-              <p>{String.fromCodePoint(0x1F5E8)}Catch Phrase: {selectedVillager['catch-phrase']}</p>
-              <p>{String.fromCodePoint(0x1F4AC)}Saying: {selectedVillager.saying}</p>
-            </div>
+          </div>
+         <div className='info'>
+          <p>Personality: {selectedVillager.personality}
+            <span className='gender'>{String.fromCodePoint(0x26A7)} {selectedVillager.gender}</span>
+          </p>
+          <p>Species: {selectedVillager.species}</p>
+          <p>{String.fromCodePoint(0x1F5E8)}Catch Phrase: {selectedVillager['catch-phrase']}</p>
+          <p>{String.fromCodePoint(0x1F4AC)}Quote: {selectedVillager.saying}</p>
          </div>
         </article>
       </section>
