b178eabb89691247855cda400b733fd8efa805f0
Thao Ma <MaThao@gmail.com>
2021-03-05 10:07:53 -0700

    Add link to back button to return home

--- src/Components/ProfilePage/ProfilePage.js
+++ src/Components/ProfilePage/ProfilePage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import apiCalls from '../../apiCalls';
 import leafGreen from '../../assets/leaf-green.png';
 import backButton from '../../assets/back-button.png';
@@ -14,32 +15,36 @@ const ProfilePage = ({match}) => {
 
   if (selectedVillager) {
     return (
-      <section className='passport-container'>
-        <img  className='back-button' src={backButton} alt='back button'/>
-        <h2>--- PASSPORT ---
-          <div className='leaf-margin'>
-            <img className='profile-leaf' src={leafGreen} alt='Leaf Icon' />
+      <section>
+        <Link to={'/'}>
+          <img className='back-button' src={backButton} alt='back button'/>
+        </Link>
+        <div className='passport-container'>
+          <h2>--- PASSPORT ---
+            <div className='leaf-margin'>
+              <img className='profile-leaf' src={leafGreen} alt='Leaf Icon' />
+            </div>
+          </h2>
+          <article className='profile-page'>
+            <div className='left-side'>
+              <img 
+                src={selectedVillager.image_uri} 
+                alt='villager profile' 
+                className='profile-picture'
+                />
+              <p>{String.fromCodePoint(0x1F9F3)}Name: {selectedVillager.name['name-USen']}</p>
+              <p>{String.fromCodePoint(0x1F330)}Born: {selectedVillager['birthday-string']}</p>
+            </div>
+          <div className='info'>
+            <p>{String.fromCodePoint(0x1F5E3)}Personality: {selectedVillager.personality}
+              <span className='gender'>{String.fromCodePoint(0x26A7)} {selectedVillager.gender}</span>
+            </p>
+            <p>{String.fromCodePoint(0x1F331)}Species: {selectedVillager.species}</p>
+            <p>{String.fromCodePoint(0x1F5E8)}Catch Phrase: {selectedVillager['catch-phrase']}</p>
+            <p>{String.fromCodePoint(0x1F4AC)}Quote: {selectedVillager.saying}</p>
           </div>
-        </h2>
-        <article className='profile-page'>
-          <div className='left-side'>
-            <img 
-              src={selectedVillager.image_uri} 
-              alt='villager profile' 
-              className='profile-picture'
-              />
-            <p>{String.fromCodePoint(0x1F9F3)}Name: {selectedVillager.name['name-USen']}</p>
-            <p>{String.fromCodePoint(0x1F330)}Born: {selectedVillager['birthday-string']}</p>
-          </div>
-         <div className='info'>
-          <p>{String.fromCodePoint(0x1F5E3)}Personality: {selectedVillager.personality}
-            <span className='gender'>{String.fromCodePoint(0x26A7)} {selectedVillager.gender}</span>
-          </p>
-          <p>{String.fromCodePoint(0x1F331)}Species: {selectedVillager.species}</p>
-          <p>{String.fromCodePoint(0x1F5E8)}Catch Phrase: {selectedVillager['catch-phrase']}</p>
-          <p>{String.fromCodePoint(0x1F4AC)}Quote: {selectedVillager.saying}</p>
-         </div>
-        </article>
+          </article>
+        </div>
       </section>
     )
   }
