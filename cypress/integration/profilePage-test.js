f05955e63c7a306ab0e8cc951aa8b0ecd5e8672b
Thao Ma <MaThao@gmail.com>
2021-03-07 18:59:48 -0700

    Add user integration test for profile page

--- cypress/integration/profilePage-test.js
+++ cypress/integration/profilePage-test.js
@@ -1,7 +1,7 @@
 describe('App', () => {
   beforeEach(() => {
     // cy.intercept('GET', 'https://acnhapi.com/v1a/villagers', {fixture: 'villagerData.json'});
-    cy.visit('http://localhost:3000/50');
+    cy.visit('http://localhost:3000/72');
   })
 
   it('should have a passport container with a header and favorite button', () => {
@@ -43,5 +43,26 @@ describe('App', () => {
 
   it('should have a paragrapgh stating Sorry, page is taking a bit to load!', () => {
     cy.get('.loader').should('have.text', 'Sorry, page is taking a bit to load!')
+  })
+
+  it.only('should test user integration', () => {
+    cy.get('.back-button')
+      .click()
+      .url()
+      .should('contain', '/');
+    cy.contains('Moe')
+      .click()
+      .url()
+      .should('contain', '/50')
+      .get('.profile-leaf')
+      .click()
+      .get('.header-leaf')
+      .click()
+      .url()
+      .should('contain', '/')
+      .get('h1')
+      .click()
+      .url()
+      .should('contain', '/');
   })
 })
--- cypress/integration/villagerList-test.js
+++ cypress/integration/villagerList-test.js
@@ -4,9 +4,9 @@ describe('App', () => {
     cy.visit('http://localhost:3000');
   })
   
-  it('should see a header, form and favorite button on page load', () => {
+  it.only('should see a header, form and favorite button on page load', () => {
     cy.get('header h1').should('have.text', 'ACNH Collections');
-    cy.get('form').find('img').should('have.attr', 'src', '/static/media/leaf-green.93f7536c.png');
+    cy.get('form').find('img').should('have.class', 'header-leaf');
     cy.get('form').and('be.visible');
   })
 
